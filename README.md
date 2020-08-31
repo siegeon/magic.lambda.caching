e1cee8d4a3ab36ae2085c9e09527e6992ab14d45
Thomas Hansen <thomas@gaiasoul.com>
2020-08-31 16:36:46 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# magic.lambda.caching
-Implements caching slots for Magic
+
+# Magic Lambda Caching
+
+[![Build status](https://travis-ci.org/polterguy/magic.lambda.caching.svg?master)](https://travis-ci.org/polterguy/magic.lambda.caching)
+
+Cache slots for Magic, more specifically the following slots.
+
+* __[cache.get]__ - Returns a previously cached item, if existing.
+* __[cache.set]__ - Adds the specified item to the cache.
+* __[cache.evict]__ - Evicts an item from the cache.
+
+## License
+
+Although most of Magic's source code is publicly available, Magic is _not_ Open Source or Free Software.
+You have to obtain a valid license key to install it in production, and I normally charge a fee for such a
+key. You can [obtain a license key here](https://servergardens.com/buy/).
+Notice, 7 days after you put Magic into production, it will stop functioning, unless you have a valid
+license for it.
+
+* [Get licensed](https://servergardens.com/buy/)
---
+++ magic.lambda.caching/CacheGet.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using Microsoft.Extensions.Caching.Memory;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.caching
+{
+    /// <summary>
+    /// [cache.get] slot returning an item from the cache, if existing.
+    /// </summary>
+    [Slot(Name = "cache.get")]
+    public class CacheGet : ISlot
+    {
+        readonly IMemoryCache _cache;
+
+        /// <summary>
+        /// Creates an instance of your type.
+        /// </summary>
+        /// <param name="cache">Actual implementation.</param>
+        public CacheGet(IMemoryCache cache)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        /// <summary>
+        /// Slot implementation.
+        /// </summary>
+        /// <param name="signaler">Signaler that raised the signal.</param>
+        /// <param name="input">Arguments to slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = _cache.Get(input.GetEx<string>());
+        }
+    }
+}
