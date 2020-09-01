87abc43f84492f44b789468c8292b18abd63153c
Thomas Hansen <thomas@gaiasoul.com>
2020-09-01 10:16:11 +0300

    Update CacheSet.cs

--- magic.lambda.caching/CacheSet.cs
+++ magic.lambda.caching/CacheSet.cs
@@ -42,19 +42,34 @@ namespace magic.lambda.caching
         {
             var key = input.GetEx<string>();
             var val = input.Children.FirstOrDefault(x => x.Name == "value")?.Value;
-            var expiration = input.Children.FirstOrDefault(x => x.Name == "expiration")?.GetEx<long>() ?? 
-                long.Parse(_configuration["magic:caching:expiration"]);
+
+            // Checking if value is null,l at which point we simply remove cached item.
             if (val == null)
             {
                 _cache.Remove(key);
+                return;
+            }
+
+            // Caller tries to actually save an object to cache.
+            var expiration = input.Children.FirstOrDefault(x => x.Name == "expiration")?.GetEx<int>() ?? 
+                int.Parse(_configuration["magic:caching:expiration"] ?? "5");
+
+            var expirationType = input.Children.FirstOrDefault(x => x.Name == "expiration-type")?.GetEx<string>() ?? 
+                _configuration["magic:caching:expiration-type"] ??
+                "sliding";
+
+            var options = new MemoryCacheEntryOptions();
+            if (expirationType == "sliding")
+            {
+                options.SlidingExpiration = new TimeSpan(0, 0, expiration);
+            }
+            else if (expirationType == "absolute")
+            {
+                options.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(expiration);
             }
             else
-            {
-                _cache.Set(key, val, new MemoryCacheEntryOptions
-                {
-                    
-                });
-            }
+                throw new ArgumentException($"'{expirationType}' is not a known type of expiration");
+            _cache.Set(key, val, options);
         }
     }
 }
